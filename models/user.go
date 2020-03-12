5dbccafd9735114c13f3af729a46cf0f637f2cae
Panos61 <panagiwtis.orovas@gmail.com>
2020-03-12 02:02:04 +0200

    edit

--- models/user.go
+++ models/user.go
@@ -8,7 +8,7 @@ import (
 type User struct {
 	gorm.Model
 
-	Username string `json:"username"`
+	Username string `gorm:"type:varchar(18);unique_index" json:"username"`
 	Email    string `gorm:"type:varchar(100);unique_index" json:"email"`
 	Gender   string `json:"gender"`
 	Password string `json:"password"`
