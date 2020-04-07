23b7d0ce7d5f0b93a88144061f0cb790fa83fcd9
Panos61 <panagiwtis.orovas@gmail.com>
2020-04-07 22:30:33 +0300

    Commit

--- api/models/profile.go
+++ api/models/profile.go
@@ -1,6 +1,12 @@
 package models
 
-import "time"
+import (
+	"html"
+	"strings"
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 // Profile struct
 type Profile struct {
@@ -10,4 +16,24 @@ type Profile struct {
 	Age          uint      `json:"age"`
 	CreatedAt    time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt    time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+
+// SaveProfile => Inserts new query in DB
+func (u *Profile) SaveProfile(db *gorm.DB) (*Profile, error) {
+	var err error
+	err = db.Debug().Create(&u).Error
+
+	if err != nil {
+		return &Profile{}, err
+	}
+
+	return u, nil
+}
+
+// Prepare //
+func (u *Profile) Prepare() {
+	u.Nickname = html.EscapeString(strings.TrimSpace(u.Nickname))
+	u.Intruduction = html.EscapeString(strings.TrimSpace(u.Intruduction))
+	u.CreatedAt = time.Now()
+	u.UpdatedAt = time.Now()
 }
