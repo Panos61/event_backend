2fa04e71942d55aecf17582dfe70dbeb59857f14
Panos61 <panagiwtis.orovas@gmail.com>
2020-05-02 15:50:39 +0300

    Routes Modified

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -1,8 +1,10 @@
 package controllers
 
+import "event_backend/api/middlewares"
+
 func (s *Server) intializeRoutes() {
 
-	v1 := s.Router.Group("/")
+	v1 := s.Router.Group("api/v1")
 	{
 		// Register Route
 		v1.POST("/register", s.CreateUser)
@@ -13,6 +15,14 @@ func (s *Server) intializeRoutes() {
 		// Users
 		v1.GET("/users", s.GetUsers)
 		v1.GET("/users/:id", s.GetUserByID)
+		v1.DELETE("/users/:id", middlewares.TokenAuthMiddleware(), s.DeleteUser)
+
+		// User Profile
+		v1.PUT("/profile", s.initProfile)
+
+		// Events
+		v1.POST("/create-event", s.CreateEvent)
+
 	}
 
 }
